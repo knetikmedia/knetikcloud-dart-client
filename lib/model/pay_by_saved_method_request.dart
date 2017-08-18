e0ee1f29e5f6840b10ae1c06ca9470208cf7d3f2
stoutfiles <shawn.stout@knetik.com>
2017-08-17 22:16:47 -0400

    JSAPI Dart API update

--- lib/model/pay_by_saved_method_request.dart
+++ lib/model/pay_by_saved_method_request.dart
@@ -7,11 +7,15 @@ class PayBySavedMethodRequest {
   @Property(name: 'payment_method')
   int paymentMethod = null;
   
+/* The id of a user to bill. Must have PAYMENTS_ADMIN permission */
+  @Property(name: 'user_id')
+  int userId = null;
+  
   PayBySavedMethodRequest();
 
   @override
   String toString()  {
-    return 'PayBySavedMethodRequest[paymentMethod=$paymentMethod, ]';
+    return 'PayBySavedMethodRequest[paymentMethod=$paymentMethod, userId=$userId, ]';
   }
 
 }
